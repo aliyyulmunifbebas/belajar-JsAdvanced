cefef63861215c074bcac60a430c229b6c340b32
aliyyulmunifbebas <rexixstore@gmail.com>
2025-11-21 11:28:58 +0700

    nambahin file js2 dan html2, belajar prototype

--- script.js
+++ script.js
@@ -34,25 +34,74 @@
 
 //constructur function
 //object.create()
-const MethodSiswa= {
-makan: function(porsi){
-    this.energi=this.energi + porsi
-},
-main: function(jam){
-    this.energi=this.energi-jam
-},
-tidur: function(jam){
-    this.energi=this.energi+jam
+// const MethodSiswa= {
+// makan: function(porsi){
+//     this.energi=this.energi + porsi
+// },
+// main: function(jam){
+//     this.energi=this.energi-jam
+// },
+// tidur: function(jam){
+//     this.energi=this.energi+jam
+// }
+// }
+// function Siswa(nama,energi){
+
+//     let siswa= Object.create(MethodSiswa)
+
+//     siswa.nama= nama
+//     siswa.energi= energi
+
+//     return siswa
+// }
+
+// let aliyyul = Siswa(`aliyyul`,10)
+
+///////////////////////////////////////////////////////////
+//_______________________________________________________//
+///////////////////////////////////////////////////////////
+
+
+// function Siswa(nama,energi){
+
+//     // let siswa= Object.create(MethodSiswa)
+
+//     this.nama= nama
+//     this.energi= energi
+
+//     // return siswa
+// }
+
+// Siswa.prototype.makan = function(porsi){
+//     this.energi += porsi
+//     return `halo ${this.nama} selamat makan`
+
+// }
+
+// Siswa.prototype.main = function(jam){
+//     this.energi -= jam
+//     return `selamat bermain ${this.nama}`
+// }
+
+// Siswa.prototype.tidur = function(jam){
+//     this.energi += jam
+//     return `tidur yang nyenyak ${this.nama}`
+// }
+
+
+// let aliyyul = new Siswa(`aliyyul`,10)
+
+class Siswa {
+    constructor(nama,energi){
+        this.nama = nama
+        this.energi = energi
+    }
+    makan(porsi){
+        this.energi += porsi
+    }
+    main(jam){
+        this.energi -= jam
+    }
 }
-}
-function Siswa(nama,energi){
 
-    let siswa= Object.create(MethodSiswa)
-
-    siswa.nama= nama
-    siswa.energi= energi
-
-    return siswa
-}
-
-let aliyyul = Siswa(`aliyyul`,10)
+let aliyyul = new Siswa(`aliyyul`,10)
